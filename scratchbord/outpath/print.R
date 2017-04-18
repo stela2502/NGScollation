f8119e693ee66d6d77ebdf5da7a766c4fbdb269f
stefanl <stefanl@aurora-scc.int>
2017-04-18 09:28:27 +0200

    Print() does not work, the print function should probably be renamed to show.

--- scratchbord/outpath/print.R
+++ scratchbord/outpath/print.R
@@ -16,7 +16,7 @@ setMethod('print', signature = c ('NGScollation'),
 	definition = function (x) {
 print (paste("An object of class",class(x)[1],"with",
 				length(names(x$objects)),"NGS datasets:"))
-lapply( names(x$objects), function(name) { 
+t <- lapply( names(x$objects), function(name) { 
 			print ( paste( 
 							name,": an object of class",class(x$objects[[name]])[1],
 							"with", ncol(x$objects[[name]]@data), "samples and",
